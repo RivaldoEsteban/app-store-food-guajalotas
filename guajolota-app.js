afaa91019aff388991beb90e0b2b504f46a48b6c
Rivaldo Esteban <63760024+RivaldoEsteban@users.noreply.github.com>
2021-03-15 16:19:29 -0600

    iniciando con el home page

---
+++ guajolota-app.js
@@ -0,0 +1,3 @@
+const guajolota =  document.querySelector('#product-guajolota')
+const bebidas = document.querySelector('#product-bebida')
+const tamales =  document.querySelector('#product-tamales')
